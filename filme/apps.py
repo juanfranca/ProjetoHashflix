09eb5f0c01c722e40c25df07c13326a3dc072499
unknown <juanfranca123@gmail.com>
2023-05-13 02:50:15 -0300

    testing the error

--- filme/apps.py
+++ filme/apps.py
@@ -5,17 +5,7 @@ class FilmesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'filme'
 
-    def ready(self):
-        from .models import Usuario
-        import os
-
-        email = os.getenv("EMAIL_ADMIN")
-        senha = os.getenv("SENHA_ADMIN")
-
-        usuarios = Usuario.objects.filter(email=email)
-        if not usuarios:
-            Usuario.objects.create_superuser(username="manager", email=email, password=senha,
-                                             is_active=True, is_staff=True)
+    
 
 
   
