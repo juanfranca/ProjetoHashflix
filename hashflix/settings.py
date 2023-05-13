89e3e1c6edada264223c3046edfa8163a10e20f2
unknown <juanfranca123@gmail.com>
2023-05-13 00:23:47 -0300

    allowed hosts

--- hashflix/settings.py
+++ hashflix/settings.py
@@ -31,7 +31,7 @@ else:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["projetohashflix-production.up.railway.app/", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["projetohashflix-production.up.railway.app", "localhost", "127.0.0.1"]
 
 
 # Application definition
